3c424b1f515529d4bcba46e3cdd5cb841ca9a0b2
T-Dimov <T-Dimov@users.noreply.github.com>
2025-12-02 15:04:49 +0200

    Add Day 2 part 1 solution

---
+++ days/src/day02.rs
@@ -0,0 +1,40 @@
+use std::fs;
+
+pub fn run()
+{
+    println!("Day 02");
+    println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
+}
+
+fn part1() -> i64
+{
+    process_file("input/day02/input.txt", |id| {
+        let id_str = id.to_string();
+        let len = id_str.len();
+        let halves = id_str.split_at(len / 2);
+        len % 2 == 0 && halves.0 == halves.1
+    })
+}
+
+fn part2() -> i64
+{
+    todo!()
+}
+
+fn process_file<P>(path: &str, is_invalid_id: P) -> i64
+where
+    P: Fn(&i64) -> bool,
+{
+    fs::read_to_string(path)
+        .unwrap()
+        .trim()
+        .split(',')
+        .map(|pair| {
+            let mut parts = pair.split('-');
+            parts.next().unwrap().parse::<i64>().unwrap()
+                ..=parts.next().unwrap().parse::<i64>().unwrap()
+        })
+        .map(|range| range.filter(&is_invalid_id).sum::<i64>())
+        .sum::<i64>()
+}
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,11 +1,12 @@
 fn main()
 {
     // Change this to run different days
-    let day = 1;
+    let day = 2;
 
     match day
     {
         1 => days::day01::run(),
+        2 => days::day02::run(),
         _ => println!("Day {} not implemented yet", day),
     }
 }
