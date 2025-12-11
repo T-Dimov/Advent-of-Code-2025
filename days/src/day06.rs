612787b249f6317160510e5f08d16457d55e8ab8
T-Dimov <T-Dimov@users.noreply.github.com>
2025-12-11 18:30:40 +0200

    Add skeletons for days 5 through 12.

---
+++ days/src/day06.rs
@@ -0,0 +1,16 @@
+pub fn run()
+{
+    println!("Day 06");
+    println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
+}
+
+fn part1() -> i64
+{
+    0
+}
+
+fn part2() -> i64
+{
+    0
+}
